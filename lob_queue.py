c4eacfcf05e1bd170929bea350bdac1c7f4ef0fe
Gabriel Dilly <gabriel_dilly@hotmail.com>
2020-06-07 19:15:27 -0300

    Added volume at price

--- lob_queue.py
+++ lob_queue.py
@@ -35,6 +35,7 @@ last_price = initial_price
 price_history = []
 bid_history = []
 ask_history = []
+volume_history = []
 bid, ask = initial_price - pip, initial_price
 mid = (bid + ask) / 2
 order_id = 0
@@ -51,8 +52,12 @@ def add_trades(total_traded, trades):
 def get_last_price(last_price, trades):
     return trades[len(trades) - 1]['price'] if len(trades) > 0 else last_price
 
+def get_traded_volume(trades):
+    return sum([trade['qty'] for trade in trades] + [0])
+
 # Loop
 for t in range(max_iterations):
+    traded_volume = 0
     
     # Limit order cancellations
     
@@ -81,6 +86,7 @@ for t in range(max_iterations):
                 limit_orders.insert(order['idNum'], order)
                 total_traded = add_trades(total_traded, trades)
                 last_price = get_last_price(last_price, trades)
+                traded_volume += get_traded_volume(trades)
     
     
     # Market order arrivals
@@ -99,6 +105,7 @@ for t in range(max_iterations):
                 trades, id_num = lob.processOrder(order, False, False)
                 total_traded = add_trades(total_traded, trades)
                 last_price = get_last_price(last_price, trades)
+                traded_volume += get_traded_volume(trades)
             
         mkt_sell = df[df['price'] == ask].to_dict(orient = 'records')[0]
         for i in range(int(np.random.poisson(mkt_sell['mu']))):
@@ -113,6 +120,7 @@ for t in range(max_iterations):
                 trades, id_num = lob.processOrder(order, False, False)
                 total_traded = add_trades(total_traded, trades)
                 last_price = get_last_price(last_price, trades)
+                traded_volume += get_traded_volume(trades)
     
     # Next prices
             
@@ -123,6 +131,7 @@ for t in range(max_iterations):
     price_history.append(last_price)
     bid_history.append(bid)
     ask_history.append(ask)
+    volume_history.append(traded_volume)
     
     # Plot chart
     
@@ -151,20 +160,47 @@ for t in range(max_iterations):
     # fig = go.Figure(data = traces, layout = layout)
     # fig.show()
     
-
 # print(lob)
+
+# Simulation Evolution
     
 traces = [
     go.Scatter(x = list(range(max_iterations)), y = price_history, name = 'Last', marker_color = 'green'),
     go.Scatter(x = list(range(max_iterations)), y = bid_history, name = 'Bid', marker_color = 'blue'),
-    go.Scatter(x = list(range(max_iterations)), y = ask_history, name = 'Ask', marker_color = 'red')
+    go.Scatter(x = list(range(max_iterations)), y = ask_history, name = 'Ask', marker_color = 'red'),
+    go.Bar(x = list(range(max_iterations)), y = volume_history, name = 'Volume', marker_color = 'rgba(0, 0, 0, 0.5)', yaxis = 'y2')
     ]
 layout = {
-    'title': 'Evolução do Preço',
+    'title': 'Evolução da Simulação',
     'separators': '.',
-    'yaxis': dict(tickformat = '.2f', gridcolor = 'grey'),
+    'yaxis': dict(tickformat = '.2f', gridcolor = 'grey', title = 'Preço', domain = [0.3, 1]),
+    'yaxis2': dict(
+        title = 'Volume',
+        side = 'right',
+        domain = [0, 0.2],
+        anchor = 'x'
+    ),
     'margin': dict(l = 40, r = 40, b = 40, t = 60, pad = 20),
     'plot_bgcolor': 'rgb(255, 255, 255)',
     }
 fig = go.Figure(data = traces, layout = layout)
 fig.show()
+
+# Volume at Price
+
+volumes = pd.DataFrame(total_traded).T
+traces = [
+    go.Bar(x = volumes['qty'], y = volumes.index, name = 'Volume', marker_color = 'green', orientation = 'h')
+    ]
+layout = {
+    'title': 'Volume por Preço',
+    'separators': '.',
+    'xaxis': dict(title = 'Quantidade', gridcolor = 'grey'),
+    'yaxis': dict(tickformat = '.2f', nticks = 10, title = 'Preço'),
+    'margin': dict(l = 40, r = 40, b = 40, t = 60, pad = 20),
+    'plot_bgcolor': 'rgb(255, 255, 255)',
+    }
+fig = go.Figure(data = traces, layout = layout)
+fig.show()
+
+
